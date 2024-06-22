c337b40ce6ec115fb18166f244d55b24f7e494dc
Your Name <you@example.com>
2024-06-22 14:56:30 +0200

    display working

--- README.md
+++ README.md
@@ -4,21 +4,54 @@ bluetooth sink / Internet Radio receiver
 based on multicore esp32 (needed by audio lib)
 use 32 bit DAC for output
 basic screen for display
-3-4 buttons for control
+2 buttons for control, short & long press
 
 optional case? (cnc)
 
 # Hardware
-## Screen
-Screen Display Size: 0.91″ 
 
-Display Resolution: 128X32 pixel 
-Working Voltage: DC 3.3-5V 
-Driver IC:SSD1306 
-Communication Interface:IIC(GND,VCC,SCL,SDA) 
+# ucrontroler
+Microcontroller |ESP32
+Frequency | 240MHz
+Flash | 4MB
+RAM | 320KB
+Vendor | Espressif
+
+[pinout](ressources\ESP-WROOM-32-Dev-Module-pinout-2117679404.jpg)
+
+## Screen
+
+Communication Lib:
+- for option 1 adafruit/Adafruit_SSD1306 but unsuitable for SH1106
+- for SH1106 olikraus/u8g2, potentially compatible with SSD1306 (untested(yet?))
+        * 0°+/*+- Dependence pb with platformio, not yet sorted
+
+### option 1 :
+Screen Display Size | 0.91″ 
+Resolution | 128X32
+Type | OLED
+Voltage | 3.3V-5V DC
+Driver IC | SSD1306 
+Communication Interface | IIC(GND,VCC,SCL,SDA) 
 from AE: (https://fr.aliexpress.com/item/1005004355547926.html)
 
+### option 2 : 
+i2c SH1106
+
+Resolution | 128x64
+Type | OLED
+Voltage | 3.3V-5V DC
+Driver IC | SH1106 
+Communication Interface | IIC(GND,VCC,SCL,SDA) 
+Module Size | 27mmx 27mm x 4mm
+
+
+[pinout](ressources\I2C-OLED-Display-Module-Pinout.jpg)
+from AE: (https://fr.aliexpress.com/item/1005004355547926.html)
+
+
 converting pbg to byte arrays: (https://mischianti.org/images-to-byte-array-online-converter-cpp-arduino/)
+or saved as XMB (for u8g2 use)
 
 # basic flow
 Démarrage mode radio
--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,32 @@ void saveConfigCallback()
   ESP_LOGI(TAG, "WifiManager data saved");
 }
 */
+
+/*
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+*/
+
+#include "U8g2lib.h" 
+
+U8G2_SH1106_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 22, /* data=*/ 21);
+const unsigned char epd_bitmap_Bitmap [] PROGMEM = {
+	// 'vol, 26x26px
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF0, 0x1F, 0x00, 0x00, 
+  0xFC, 0x7F, 0x00, 0x00, 0xFE, 0xFF, 0x00, 0x00, 0xFE, 0xFF, 0x00, 0x00, 
+  0xFE, 0xFC, 0x00, 0x00, 0x7E, 0xFC, 0x30, 0x00, 0xFE, 0xFC, 0x70, 0x00, 
+  0xFE, 0xFF, 0xF6, 0x00, 0xFE, 0xFF, 0xFE, 0x01, 0xFE, 0xFF, 0xFC, 0x01, 
+  0xFE, 0xFF, 0x78, 0x00, 0x3E, 0xFB, 0x78, 0x00, 0xFE, 0xF7, 0xFC, 0x01, 
+  0xDE, 0xF7, 0xFE, 0x01, 0xDE, 0xFF, 0xF6, 0x00, 0xDE, 0xF7, 0x70, 0x00, 
+  0xBE, 0xFF, 0x30, 0x00, 0x7E, 0xFC, 0x00, 0x00, 0xFE, 0xFF, 0x00, 0x00, 
+  0xFC, 0xFF, 0x00, 0x00, 0xFC, 0x7F, 0x00, 0x00, 0xF0, 0x1F, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };
+
 void setup()
 {
   Serial.begin(115200);
@@ -46,14 +72,62 @@ void setup()
            wifiManager.getWiFiSSID(),
            wifiManager.getWiFiHostname());
            */
+
+
+/*
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+
+      ESP_LOGE(TAG, "ERROR LCD");
+
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+        ESP_LOGI(TAG, "LCD OK");
+
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+display.clearDisplay();
+display.drawCircle(20,20,10,SSD1306_WHITE);
+display.drawCircle(40,40,10,SSD1306_BLACK);
+
+display.display();
+*/
+  u8g2.begin();
+    u8g2.firstPage();
+
+u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);   
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0,0,"Hello World!");
+    u8g2.setDrawColor(1);
+    u8g2.drawXBM(10, 10, 26, 26, epd_bitmap_Bitmap);
+
+  } while ( u8g2.nextPage() );
+
+
   ESP_LOGD(TAG, "End INIT");
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  sleep(1);
   /*
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0,24,"Hello World!");
+  } while ( u8g2.nextPage() );
+  */
+  
+    /*
   if (xQueueReceive(button_events, &ev, 1000/portTICK_PERIOD_MS)) {
         if ((ev.pin == BUTTON_1) && (ev.event == BUTTON_DOWN)) {
             // ...
@@ -63,4 +137,8 @@ void loop()
         }
     }
   */
+
+  ESP_LOGD(TAG, "LOOP");
+  delay(2000);
+
 }
